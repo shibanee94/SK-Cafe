708acb7bf46d16637c0894588e064677401e6141
shibanee94 <shibanee.sai94@gmail.com>
2022-11-30 16:02:23 +0530

    added header and sub header component

--- skcafe/src/container/Header/Header.jsx
+++ skcafe/src/container/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { SubHeading  } from '../../components';
+import { images } from "../../constants";
 
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The Key to Boost Your Mood</h1>
+      <p className="open__opensans" style={{ margin: `2rem 0`}}>lorem gtug ufjh bvjhgffg juy oiy bdgfg getuy dpofivfjj bvhgjgf bfhgfjhfj.</p>
+      <button type="button" className="custom__buttom">Explore Menu</button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header img" />
+    </div>
+    
   </div>
 );
 
