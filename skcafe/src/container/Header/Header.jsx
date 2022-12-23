85c513b676eea4bc87c41917a7d8b3f1b7324448
shibanee94 <shibanee.sai94@gmail.com>
2022-12-23 16:24:07 +0530

    changes some texts

--- skcafe/src/container/Header/Header.jsx
+++ skcafe/src/container/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => (
       <SubHeading title="Chase the new flavour" />
       <h1 className="app__header-h1">The Key to Boost Your Mood</h1>
       <img src={images.spoon} alt="spoon" className="spoon__img" />
-      <p className="open__opensans" style={{ margin: `2rem 0`}}>lorem gtug ufjh bvjhgffg juy oiy bdgfg getuy dpofivfjj bvhgjgf bfhgfjhfj.</p>
+      <p className="open__opensans" style={{ margin: '2rem 0'}}>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups. Velit Esse Cillum Dolore Eu Fugiat Nulla Pariatur. Excepteur Sint Occaecat Cupidatat Non Proident, Sunt In Culpa Qui Officia Deserunt Mollit Anim Id Est Laborum.</p>
       <button type="button" className="custom__buttom">Explore Menu</button>
     </div>
     <div className="app__wrapper_img">
